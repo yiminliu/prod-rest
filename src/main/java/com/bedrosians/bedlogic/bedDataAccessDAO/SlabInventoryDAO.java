2a27464edd181ec1091022033005cfb75212d2ae
Francis Nguyen <francistn@gmail.com>
2014-02-05 13:33:01 -0800

    Add slab inventory resource

---
+++ src/main/java/com/bedrosians/bedlogic/bedDataAccessDAO/SlabInventoryDAO.java
@@ -0,0 +1,33 @@
+package com.bedrosians.bedlogic.bedDataAccessDAO;
+
+import com.thetransactioncompany.jsonrpc2.client.*;
+import com.thetransactioncompany.jsonrpc2.*;
+import net.minidev.json.JSONObject;
+
+import com.bedrosians.bedlogic.bedDataAccessDAO.JSONRPCDAO;
+import com.bedrosians.bedlogic.exception.BedDAOException;
+import com.bedrosians.bedlogic.models.SlabInventory;
+
+public class SlabInventoryDAO
+{
+    public SlabInventoryDAO()
+    {
+    }
+        
+    public SlabInventory getSlabInventory(String userType, String userCode, String itemCode, String locationCode, String unit)
+        throws BedDAOException
+    {        
+        JSONRPCDAO  rpcDAO = JSONRPCDAO.Create();
+        
+        rpcDAO.setMethodName("readSlabInventory");
+        rpcDAO.setMethodResultType("slabinventory");
+        rpcDAO.addStringParameter(userType);
+        rpcDAO.addStringParameter(userCode);
+        rpcDAO.addStringParameter(itemCode);
+        rpcDAO.addStringParameter(locationCode);
+        rpcDAO.addStringParameter(unit);
+        JSONObject  result = rpcDAO.call();
+        
+        return new SlabInventory(result);
+    }
+}
