330f50c48cd6b5f1425b855625dae714ba2ed277
Francis Nguyen <francistn@gmail.com>
2014-02-05 08:52:19 -0800

    Add inventory resource

---
+++ src/main/java/com/bedrosians/bedlogic/resources/InventoryResource.java
@@ -0,0 +1,65 @@
+package com.bedrosians.bedlogic.resources;
+
+import javax.ws.rs.Path;
+import javax.ws.rs.GET;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.HttpHeaders;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+import javax.ws.rs.WebApplicationException;
+
+import com.bedrosians.bedlogic.exception.BedDAOException;
+import com.bedrosians.bedlogic.bedDataAccessDAO.InventoryDAO;
+import com.bedrosians.bedlogic.models.Inventory;
+
+@Path("/inventory")
+public class InventoryResource
+{
+    /**
+     * Inventory resource
+     * Query Params
+     * - unit:
+     */
+    @GET
+    @Produces({MediaType.APPLICATION_JSON})
+    @Path("{itemcode}")
+    public Response getInventory(@Context HttpHeaders requestHeaders
+                                    , @PathParam("itemcode") String itemCode
+                                    , @QueryParam("unit") String unit)
+    {
+        Response    response;
+
+        try
+        {
+            // Check usercode
+            UserCodeParser  userCodeParser = new UserCodeParser(requestHeaders);
+            if (!userCodeParser.isValidFormat())
+            {
+                throw new WebApplicationException(Response.Status.UNAUTHORIZED);
+            }
+            String userType = userCodeParser.getUserType();
+            String userCode = userCodeParser.getUserCode();
+            
+            // Get query params
+            unit = (unit == null) ? "" : unit;
+            
+            // Retrieve DAO object
+            InventoryDAO  inventoryDAO = new InventoryDAO();
+            Inventory     result = inventoryDAO.getInventory(userType, userCode, itemCode, unit);
+            
+            // Return json reponse
+            String  jsonStr = result.toJSONString();
+            response = Response.ok(jsonStr, MediaType.APPLICATION_JSON).build();
+        }
+        catch (BedDAOException e)
+        {
+            response = BedDAOExceptionMapper.MapToResponse(e);
+        }
+        
+        return response;
+    }
+}
