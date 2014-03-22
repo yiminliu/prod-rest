a2a1f4ec951900cffb1ee1d1e68f1621941f7ac0
yiminliu <yiminliu@192.168.125.107>
2014-03-21 17:42:25 -0700

    added new constructer and toJSONStringWithJackson() method to employ Jackson json to handle circular objects which cannot be handled by smart-json

--- src/main/java/com/bedrosians/bedlogic/models/Products.java
+++ src/main/java/com/bedrosians/bedlogic/models/Products.java
@@ -1,18 +1,48 @@
 package com.bedrosians.bedlogic.models;
 
+import java.io.StringWriter;
+import java.io.Writer;
+
+import org.codehaus.jackson.map.ObjectMapper;
+
+import com.bedrosians.bedlogic.exception.BedDAOException;
+
 import net.minidev.json.JSONObject;
 
 public class Products
 {
     private JSONObject  data;
     
+    private Object obj;
+    
     public Products(JSONObject inData)
     {
         this.data = inData;
     }
     
+    public Products(Object obj)
+    {
+        this.obj = obj;
+    }
+    
     public String toJSONString()
     {
         return this.data.toString();
+    }
+    
+    public String toJSONStringWithJackson() throws BedDAOException
+    {
+       ObjectMapper mapper = new ObjectMapper();
+       Writer strWriter = new StringWriter();
+       try{
+           mapper.writeValue(strWriter, obj);
+   	   }
+       catch(Exception e)
+       {
+   	      e.printStackTrace();
+   	      throw new BedDAOException(e);
+   	   }
+        return strWriter.toString();
+ 
     }
 }
