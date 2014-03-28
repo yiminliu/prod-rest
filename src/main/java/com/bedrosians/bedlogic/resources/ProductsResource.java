2101fd755ad7830f7fef3a12df5cb708a9d1f31e
yiminliu <yiminliu@192.168.125.60>
2014-03-27 18:57:34 -0700

    added more methods and error conditions

--- src/main/java/com/bedrosians/bedlogic/resources/ProductsResource.java
+++ src/main/java/com/bedrosians/bedlogic/resources/ProductsResource.java
@@ -3,11 +3,11 @@ package com.bedrosians.bedlogic.resources;
 
 import java.util.List;
 
-
 import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.GET;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.HttpHeaders;
@@ -20,6 +20,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 import com.bedrosians.bedlogic.domain.item.Item;
+import com.bedrosians.bedlogic.exception.BedDAOBadParamException;
 import com.bedrosians.bedlogic.exception.BedDAOException;
 import com.bedrosians.bedlogic.exception.BedResException;
 import com.bedrosians.bedlogic.exception.BedResUnAuthorizedException;
@@ -61,13 +62,17 @@ public class ProductsResource
             //ProductsDAO productsDAO = new ProductsDAO();
             //Products    result = productsDAO.readProductsByQueryParams(userType, userCode, queryParams);
             
-            List<Item> items = productService.getByQueryParameters(queryParams);
+            List<Item> items = productService.getProductsByQueryParameters(queryParams);
                 
             Products result = new Products(items);
             String jsonStr = result.toJSONStringWithJackson();
        
             //Return json reponse
              response = Response.ok(jsonStr, MediaType.APPLICATION_JSON).build();
+        }
+        catch (BedDAOBadParamException e)
+        {
+        	response = BedDAOExceptionMapper.MapToResponse(e);
         }
         catch (BedDAOException e)
         {
@@ -77,9 +82,53 @@ public class ProductsResource
         {
             response = BedResExceptionMapper.MapToResponse(e);
         }
-        
         return response;
     }
+	
+	@GET
+	@Path("{itemCode}")
+    @Produces({MediaType.APPLICATION_JSON})
+    public Response getProductById(@Context HttpHeaders requestHeaders, @PathParam("itemCode") final String itemCode)
+    {
+        Response    response;
+
+        try
+        {
+            // Check usercode
+            UserCodeParser  userCodeParser = new UserCodeParser(requestHeaders);
+            if (!userCodeParser.isValidFormat())
+            {
+                throw new BedResUnAuthorizedException();
+            }
+            String userType = userCodeParser.getUserType();
+            String userCode = userCodeParser.getUserCode();
+                             
+            //ProductsDAO productsDAO = new ProductsDAO();
+            //Products    result = productsDAO.readProductsByQueryParams(userType, userCode, queryParams);
+            
+            Item item = productService.getProductById(itemCode);
+                
+            Products result = new Products(item);
+            String jsonStr = result.toJSONStringWithJackson();
+       
+            //Return json reponse
+             response = Response.ok(jsonStr, MediaType.APPLICATION_JSON).build();
+        }
+        catch (BedDAOBadParamException e)
+        {
+        	response = BedDAOExceptionMapper.MapToResponse(e);
+        }
+        catch (BedDAOException e)
+        {
+            response = BedDAOExceptionMapper.MapToResponse(e);
+        }
+        catch (BedResException e)
+        {
+            response = BedResExceptionMapper.MapToResponse(e);
+        }
+        return response;
+    }
+	
 	
 	@POST
     @Produces({MediaType.APPLICATION_JSON})
@@ -111,6 +160,10 @@ public class ProductsResource
             // Return json reponse
             response = Response.ok(jsonStr, MediaType.APPLICATION_JSON).build();
         }
+        catch (BedDAOBadParamException e)
+        {
+        	response = BedDAOExceptionMapper.MapToResponse(e);
+        }
         catch (BedDAOException e)
         {
             response = BedDAOExceptionMapper.MapToResponse(e);
@@ -121,7 +174,7 @@ public class ProductsResource
         }
         
         return response;
-        }
+    }
 	
 	@PUT
     @Produces({MediaType.APPLICATION_JSON})
@@ -149,6 +202,11 @@ public class ProductsResource
             productService.updateProduct(queryParams);     
             // Return json reponse
             response = Response.ok(MediaType.APPLICATION_JSON).build();
+        }    
+            catch (BedDAOBadParamException e)
+        {
+           	response = BedDAOExceptionMapper.MapToResponse(e);
+        
         }
         catch (BedResException e)
         {
