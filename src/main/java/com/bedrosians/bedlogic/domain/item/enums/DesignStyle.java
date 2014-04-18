91776b71767271e39d80024e3f14275ca4a07bfc
yiminliu <yiminliu@192.168.125.111>
2014-04-18 10:54:44 -0700

    code cleanup

--- src/main/java/com/bedrosians/bedlogic/domain/item/enums/DesignStyle.java
+++ src/main/java/com/bedrosians/bedlogic/domain/item/enums/DesignStyle.java
@@ -2,67 +2,35 @@ package com.bedrosians.bedlogic.domain.item.enums;
 
 public enum DesignStyle {
 
-		 CONTEMPORARY("Contemporary"),
-	     MODERN("Modern"),
-	     ELECTIC("Eclectic"),
-	     TRADITIONAL("Traditional"),
-  	     ASIAN("Asian"),
-	     BEACH_STYLE("Beach Style"),
-	     CRAFTSMAN("Craftsman"),
-	     INDUSTRIAL("Industrial"),
-	     MEDITERRANEAN("Mediterranean"),
-	     MIDCENTURY("Midcentury"),
-	     RUSTIC("Rustic"),
-	     TROPICAL("Tropical");
+	Contemporary("Contemporary"),
+	Modern("Modern"),
+	Eclectic("Eclectic"),
+	Traditional("Traditional"),
+	Asian("Asian"),
+	Beach_Style("Beach Style"),
+	Craftsman("Craftsman"),
+	Industrial("Industrial"),
+	Mediterranean("Mediterranean"),
+	Midcentury("Midcentury"),
+	Rustic("Rustic"),
+	Tropical("Tropical");
+		 	   
+    private String description;
 		 
-	   
-		 private String description;
+	private DesignStyle(String description){
+		 this.description = description;
+	}
+	
+	public String getDescription(){
+		 return description;
+	}
 		 
-		 private DesignStyle(String description){
-			 this.description = description;
-		 }
-		 public String getDescription(){
-			 return description;
-		 }
-		 
-		 public static DesignStyle instanceOf(String description){
-			 DesignStyle instance = null;
-			 switch(description) {
-				 case("Contemporary"): case("CONTEMPORARY"):
-					 instance = CONTEMPORARY;
-				     break;
-				 case("Modern"): case("MODEN"):
-					 instance = MODERN;
-				     break; 
-				 case("Eclectic"): case("ELECTIC"):
-					 instance = ELECTIC;
-				     break;
-				 case("Asian"): case("ASIAN"):
-					 instance = ASIAN;
-				     break; 
-				 case("Beach Style"): case("BEACH_STYLE"):
-					 instance = BEACH_STYLE;
-				     break;
-				 case("Craftsman"): case("CRAFTSMAN"):
-					 instance = CRAFTSMAN;
-				     break; 
-				 case("Industrial"): case("INDUSTRIAL"):
-					 instance = CONTEMPORARY;
-				     break;
-				 case("Mediterranean"): case("MEDITERRANEAN"):
-					 instance = MEDITERRANEAN;
-				     break; 
-				 case("Midcentury"): case("MIDCENTURY"):
-					 instance = MIDCENTURY;
-				     break; 
-				 case("Rustic"): case("RUSTIC"):
-					 instance = RUSTIC;
-				     break;
-				 case("Tropical"): case("TROPICAL"):
-					 instance = MEDITERRANEAN;
-				     break;      
-			 }	   
-			 return instance;
-		 }
+	public static DesignStyle instanceOf(String key){
+	  for(DesignStyle designStyle : values()){
+		  if(designStyle.getDescription().equalsIgnoreCase(key))
+		     return designStyle;				        
+	  }
+	  return null;
+	}
 
 }
