e3914df1f146dd77aefe8981b960e980e16251e0
yiminliu <yiminliu@192.168.125.111>
2014-04-18 17:47:19 -0700

    added process(Item)

--- src/main/java/com/bedrosians/bedlogic/util/FormatUtil.java
+++ src/main/java/com/bedrosians/bedlogic/util/FormatUtil.java
@@ -1,8 +1,17 @@
 package com.bedrosians.bedlogic.util;
 
 import java.math.BigDecimal;
+import java.text.DecimalFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import com.bedrosians.bedlogic.domain.authority.Role;
+import com.bedrosians.bedlogic.domain.item.Item;
+import com.bedrosians.bedlogic.domain.user.User;
+import com.bedrosians.bedlogic.domain.item.embeddable.Purchasers;
 
 public class FormatUtil {
   
@@ -47,7 +56,7 @@ public class FormatUtil {
 	}
 	
 	public static Float process(Float data){
-		Float newData = Float.valueOf(0);
+		Float newData = Float.valueOf(0); 
 		if (data != null)
 			newData = Float.valueOf((String.valueOf(data).trim()));
 	    return Math.round(newData * 100)/100.00f;
@@ -58,18 +67,108 @@ public class FormatUtil {
 		if (data != null)
 			//newData = BigDecimal.valueOf((String.valueOf(data).trim()));
 			newData = data;
-	    return newData.setScale(2, BigDecimal.ROUND_HALF_EVEN);
+	    return newData.setScale(4, BigDecimal.ROUND_HALF_EVEN);
 	}
 	   
 	public static Date process(Date date) {
 					
 		if (date == null) {
-			Date emptyDate = new Date();
-			emptyDate.setTime(0);
-			return emptyDate;
+			//Date emptyDate = new Date();
+			//emptyDate.setTime(0);
+			//return emptyDate;
+			return null;
 		}	
 		else
 			return date;
+	}
+	
+	public static Item process(Item item){
+		/*Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		if(auth != null) {
+			User user = (User)auth.getPrincipal();
+		
+		   //if(!auth.getPrincipal().getAuthorities().containsAll(Role.getRolesAbove(Role.ROLE_ADMIN, true))) {
+		   if(!Role.getRolesAbove(Role.ROLE_ADMIN, true).contains(user.getGreatestAuthority())) {
+		      //item..setVendorlandedbasecost(null);
+		      item.setCost(null);
+		      item.setListpricemarginpct(null);
+		      //item.setPoincludeinvendorcost(null);
+		      item.getPrice().setMinimalMarginPct(null);
+		      item.getPrice().setPriceMarginPct(null);   
+		   }
+		}   
+		//Buyer buyer = item.getBuyer();
+		//process(item.getBuyer().getBuyer());
+		*/
+		item.setItemcode(process(item.getItemcode()));
+		return item;
+		/*
+		newItem.setAbccd(process(item.getAbccd()));
+		newItem.setCategory(process(item.getCategory()));
+		newItem.setColorCategory(process(item.getColorCategory()));
+		newItem.setColor(process(item.getColor()));
+		newItem.setDescription(process(item.getDescription()));
+		newItem.setDirectship(process(item.getDirectship()));
+		newItem.setDropdate(process(item.getDropdate()));
+		newItem.setDutypct(process(item.getDutypct()));
+		newItem.setFulldesc(process(item.getFulldesc()));
+		newItem.setIcons(process(item.getIcons()));
+		newItem.setInactivecd(process(item.getInactivecd()));
+		newItem.setInventoryItemCode(process(item.getInventoryItemCode()));
+		newItem.setItemcd2(process(item.getItemcd2()));
+		newItem.setItemCode(process(item.getItemCode()));
+		newItem.setItemdesc2(process(item.getItemdesc2()));
+		newItem.setItemgroupnbr(process(item.getItemgroupnbr()));
+		newItem.setItemtypecd(process(item.getItemtypecd()));
+		newItem.setLeadtime(process(item.getLeadtime()));
+		newItem.setLength(process(item.getLength()));
+		newItem.setListpricemarginpct(process(item.getListpricemarginpct()));
+		newItem.setLottype(process(item.getLottype()));
+		//newItem.setMaterialCategory(process(item.getMaterialCategory()));
+        //newItem.setMaterialClassCode(process(item.getMaterialClassCode()));
+		//newItem.setMaterialStyle(process(item.getMaterialStyle()));
+		//newItem.setMaterialType(process(item.getMaterialType()));
+		newItem.setMaterialFeature(process(item.getMaterialFeature()));
+		newItem.setNominalLength(process(item.getNominalLength()));
+		newItem.setNominalThickness(process(item.getNominalThickness()));
+		newItem.setNominalWidth(process(item.getNominalWidth()));		
+		newItem.setNotes1(process(item.getNotes1()));
+		newItem.setNotes2(process(item.getNotes2()));
+		newItem.setNotes3(process(item.getNotes3()));
+		newItem.setOffShade(process(item.getOffShade()));
+		newItem.setOrigin(process(item.getOrigin()));
+		newItem.setPoNotes(process(item.getPoNotes()));
+		newItem.setPrintlabel(process(item.getPrintlabel()));
+		newItem.setPriorlastupdated(process(item.getPriorlastupdated()));
+		newItem.setProductline(process(item.getProductline()));
+		newItem.setSeriesName((process(item.getSeriesName())));		
+		newItem.setShadeVariation(process(item.getShadeVariation()));
+		newItem.setShowOnAlysedwards(process(item.getShowOnAlysedwards()));
+		newItem.setShowOnWebsite(process(item.getShowOnWebsite()));
+		newItem.setSizeUnits(process(item.getSizeUnits()));
+		newItem.setSubtype(process(item.getSubtype()));
+		newItem.setThickness(process(item.getThickness()));
+		newItem.setThicknessUnit(process(item.getThicknessUnit()));
+		newItem.setType(process(item.getType()));
+		newItem.setUpdatecd(process(item.getUpdatecd()));
+		newItem.setVendordiscpct(process(item.getVendordiscpct()));
+		newItem.setVendordiscpct2(process(item.getVendordiscpct2()));
+		newItem.setVendordiscpct3(process(item.getVendordiscpct3()));	
+		newItem.setVendorfob(process(item.getVendorfob()));
+		newItem.setVendorfreightratecwt(process(item.getVendorfreightratecwt()));
+		newItem.setVendorLandedBaseCost(process(item.getVendorLandedBaseCost()));
+		newItem.setVendorlistprice(process(item.getVendorlistprice()));
+		newItem.setVendormarkuppct(process(item.getVendormarkuppct()));
+		newItem.setVendornbr(process(item.getVendornbr()));
+		newItem.setVendornbr1(process(item.getVendornbr1()));
+		newItem.setVendornbr2(process(item.getVendornbr2()));
+		newItem.setVendornbr3(process(item.getVendornbr3()));
+		newItem.setVendornetprice(process(item.getVendornetprice()));
+		newItem.setVendorpriceunit(process(item.getVendorpriceunit()));
+		newItem.setVendorroundaccuracy(process(item.getVendorroundaccuracy()));
+		newItem.setVendorxrefcd(process(item.getVendorxrefcd()));	
+		newItem.setWidth(process(item.getWidth()));
+		*/
 	}
 	
 	/*
