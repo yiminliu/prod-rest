0f84db2be681c8624de35e20598a207c65842e17
yiminliu <yiminliu@192.168.125.138>
2014-05-06 16:31:49 -0700

    new class

---
+++ src/main/java/com/bedrosians/bedlogic/domain/item/embeddable/Series.java
@@ -0,0 +1,34 @@
+package com.bedrosians.bedlogic.domain.item.embeddable;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+
+import com.bedrosians.bedlogic.util.FormatUtil;
+
+@Embeddable
+public class Series implements java.io.Serializable {
+
+	private static final long serialVersionUID = -582221787L;
+	
+	private String seriesname;
+	private String seriescolor;
+	
+	@Column(name = "seriesname", length = 40)
+	public String getSeriesname() {
+		return FormatUtil.process(this.seriesname);
+	}
+
+	public void setSeriesname(String seriesname) {
+		this.seriesname = seriesname;
+	}
+	
+	@Column(name = "color", length = 30)
+	public String getSeriescolor() {
+		return FormatUtil.process(this.seriescolor);
+	}
+
+	public void setSeriescolor(String seriescolor) {
+		this.seriescolor = seriescolor;
+	}
+
+}
