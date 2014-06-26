a433da977c1f75d559f5984aa2b2582ef0a493bb
yiminliu <yiminliu@192.168.125.116>
2014-06-25 18:09:02 -0700

    Removed unused methods

--- src/main/java/com/bedrosians/bedlogic/util/JsonUtil.java
+++ src/main/java/com/bedrosians/bedlogic/util/JsonUtil.java
@@ -2,11 +2,8 @@ package com.bedrosians.bedlogic.util;
 
 import java.io.File;
 import java.io.IOException;
-import java.math.BigDecimal;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
-import java.util.Set;
 
 import javax.ws.rs.core.MultivaluedMap;
 
@@ -17,34 +14,7 @@ import org.codehaus.jackson.map.ObjectMapper;
 import org.codehaus.jackson.type.TypeReference;
 import org.codehaus.jettison.json.JSONObject;
 
-import com.bedrosians.bedlogic.domain.item.IconCollection;
-import com.bedrosians.bedlogic.domain.item.ItemNewFeature;
-import com.bedrosians.bedlogic.domain.item.Item;
-import com.bedrosians.bedlogic.domain.item.ItemVendor;
-import com.bedrosians.bedlogic.domain.item.embeddable.Applications;
-import com.bedrosians.bedlogic.domain.item.embeddable.Cost;
-import com.bedrosians.bedlogic.domain.item.embeddable.Dimensions;
-import com.bedrosians.bedlogic.domain.item.embeddable.Material;
-import com.bedrosians.bedlogic.domain.item.embeddable.Notes;
-import com.bedrosians.bedlogic.domain.item.embeddable.PackagingInfo;
-import com.bedrosians.bedlogic.domain.item.embeddable.Price;
-import com.bedrosians.bedlogic.domain.item.embeddable.PriorVendor;
-import com.bedrosians.bedlogic.domain.item.embeddable.Purchasers;
-import com.bedrosians.bedlogic.domain.item.embeddable.Series;
-import com.bedrosians.bedlogic.domain.item.embeddable.SimilarItemCode;
-import com.bedrosians.bedlogic.domain.item.embeddable.TestSpecification;
-import com.bedrosians.bedlogic.domain.item.embeddable.Units;
-import com.bedrosians.bedlogic.domain.item.embeddable.VendorInfo;
-import com.bedrosians.bedlogic.domain.item.enums.DesignLook;
-import com.bedrosians.bedlogic.domain.item.enums.DesignStyle;
-import com.bedrosians.bedlogic.domain.item.enums.Edge;
-import com.bedrosians.bedlogic.domain.item.enums.Grade;
-import com.bedrosians.bedlogic.domain.item.enums.Status;
-import com.bedrosians.bedlogic.domain.item.enums.SurfaceApplication;
-import com.bedrosians.bedlogic.domain.item.enums.SurfaceFinish;
-import com.bedrosians.bedlogic.domain.item.enums.SurfaceType;
 import com.bedrosians.bedlogic.exception.BedDAOBadParamException;
-import com.bedrosians.bedlogic.models.Products;
 import com.sun.jersey.core.util.MultivaluedMapImpl;
 
 public class JsonUtil {
@@ -142,25 +112,6 @@ public class JsonUtil {
 			e.printStackTrace();
 		}		
 	}
-	/*
-	public static Object PojoToJsonObject(JSONObject jsonObj, Object obj){
-		Object object = null; 
-		ObjectMapper mapper = new ObjectMapper();
-		try{
-			object = mapper.writeValue(jsonObj, obj.getClass());
-		}
-        catch (JsonGenerationException e) {
-	      	e.printStackTrace();
-        } 
-		catch (JsonMappingException e) { 
-		    e.printStackTrace();
-        }
-		catch(Exception e){
-			e.printStackTrace();
-		}
-		return object;
-	}
-    */
 	
 	public static Map<String, String> JsonStringToMap(String jsonString){
 		Map<String, String> map = new HashMap<>();
@@ -214,23 +165,6 @@ public class JsonUtil {
 			e.printStackTrace();
 		}
 		return jsonString;
-	}
-	
-	public static void mapToJsonFile(Map map, String fileName){
-	    
-		ObjectMapper mapper = new ObjectMapper();
-		try{
-			mapper.writeValue(new File(fileName), map);
-		}
-        catch (JsonGenerationException e) {
-	      	e.printStackTrace();
-        } 
-		catch (JsonMappingException e) { 
-		    e.printStackTrace();
-        }
-		catch(Exception e){
-			e.printStackTrace();
-		}
 	}
 	
 	public static String getItemCode(JSONObject inputJsonObj){
