7319b48762c70625e6257c87f539e4e2a625284a
yiminliu <yiminliu@192.168.125.11>
2014-07-11 18:36:11 -0700

    Relocated the class

---
+++ src/main/java/com/bedrosians/bedlogic/service/system/IndexService.java
@@ -0,0 +1,34 @@
+package com.bedrosians.bedlogic.service.system;
+
+import javax.annotation.PostConstruct;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.search.FullTextSession;
+import org.hibernate.search.Search;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@Component("indexUtil")
+@Scope("singleton")
+public class IndexService {
+
+	@Autowired
+    private SessionFactory sessionFactory;
+	
+	/* This method is used to initialize Lucene indexes for the existing data in database.
+	 * The @PostConstruct annotation ensure the indexes are initialized when the app starts up*/
+	//@PostConstruct
+    public  boolean initializeIndex(){
+    	FullTextSession fullTextSession = Search.getFullTextSession(sessionFactory.openSession());
+    	try{
+    		fullTextSession.createIndexer().purgeAllOnStart(true).batchSizeToLoadObjects(100).optimizeOnFinish(true).startAndWait();
+    		System.out.println("Lucene indeses have initialized.");
+    		return true;
+    	}
+    	catch(InterruptedException e){
+    		System.out.println("Lucene indeses initilization failed, due to " + e.getMessage());
+    		return false;
+    	}
+    }
+}
