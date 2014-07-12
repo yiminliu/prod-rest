29d703794a6b901ec6993fbffa1b0f212ab4c668
yiminliu <yiminliu@192.168.125.11>
2014-07-11 18:34:35 -0700

    New account service classes

--- src/main/java/com/bedrosians/bedlogic/service/account/AccountService.java
+++ src/main/java/com/bedrosians/bedlogic/service/account/AccountService.java
@@ -8,25 +8,27 @@ import org.codehaus.jettison.json.JSONObject;
 import org.springframework.stereotype.Service;
 
 import com.bedrosians.bedlogic.domain.account.Account;
-import com.bedrosians.bedlogic.domain.account.AccountBranch;
-import com.bedrosians.bedlogic.domain.account.AccountBranchDetail;
-import com.bedrosians.bedlogic.domain.account.AccountDetail;
-import com.bedrosians.bedlogic.domain.account.BranchPK;
+import com.bedrosians.bedlogic.domain.account.Branch;
 import com.bedrosians.bedlogic.domain.account.CheckPayment;
 import com.bedrosians.bedlogic.exception.BedDAOBadParamException;
 import com.bedrosians.bedlogic.exception.BedDAOException;
-import com.bedrosians.bedlogic.exception.BedResException;
+import com.bedrosians.bedlogic.util.JsonWrapper.AccountWrapper;
 
 @Service
 public interface AccountService {
 	
-	public List<Account> getAccountsByCustomerCode(String customerCode) throws BedDAOBadParamException, BedDAOException;
-	public List<Account> getAccounts(MultivaluedMap<String, String> queryParams)throws BedDAOException;
-	public String createAccount(JSONObject inputJsonObj) throws BedDAOBadParamException, BedDAOException, BedResException;
-	//public void updateAccount(Account account);
+	//------------------- Account CRUD Operations ------------------//
+	public List<Account> getAccountsByCustomerCodeTest(String customerCode) throws BedDAOBadParamException, BedDAOException;
+	public List<AccountWrapper> getAccountsByCustomerCode(String customerCode) throws BedDAOBadParamException, BedDAOException;
+	public  List<AccountWrapper> getAccounts(MultivaluedMap<String, String> queryParams)throws BedDAOException;
+	public String createAccount(JSONObject jsonObj) throws BedDAOBadParamException, BedDAOException;
+	public void updateAccount(JSONObject jsonObj) throws BedDAOBadParamException, BedDAOException;
 	//public void updateAccount(String accountId, Account account) throws BedDAOException;
-	public List<? extends AccountBranch> getAccountBranches(String customerCode);
+	
+	//------------------- Branch CRUD Operations ------------------//
+	public List<? extends Branch> getBranches(String customerCode);
 	//AccountBranchDetail getAccountBranchById(String accountId, String branchId);
 	//AccountBranchDetail getAccountBranchByBranchPK(BranchPK branchId);
+	public Branch getBranch(String coustomerCode, String branchCode);
 	List<CheckPayment> getCheckPaymentsForAccount(String custcd);
 }
--- src/main/java/com/bedrosians/bedlogic/service/account/AccountServiceImpl.java
+++ src/main/java/com/bedrosians/bedlogic/service/account/AccountServiceImpl.java
@@ -8,6 +8,7 @@ import javax.ws.rs.core.MultivaluedMap;
 
 import org.codehaus.jettison.json.JSONObject;
 import org.hibernate.HibernateException;
+import org.hibernate.QueryParameterException;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,23 +16,25 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.bedrosians.bedlogic.dao.account.AccountBranchDao;
-import com.bedrosians.bedlogic.dao.account.AccountBranchDetailDao;
 import com.bedrosians.bedlogic.dao.account.AccountDao;
-import com.bedrosians.bedlogic.dao.account.AccountDetailDao;
 import com.bedrosians.bedlogic.dao.account.CheckPaymentDao;
 import com.bedrosians.bedlogic.domain.account.Account;
-import com.bedrosians.bedlogic.domain.account.AccountBranchDetail;
-import com.bedrosians.bedlogic.domain.account.AccountDetail;
-import com.bedrosians.bedlogic.domain.account.AccountBranch;
+import com.bedrosians.bedlogic.domain.account.Branch;
 import com.bedrosians.bedlogic.domain.account.BranchPK;
 import com.bedrosians.bedlogic.domain.account.CheckPayment;
-import com.bedrosians.bedlogic.domain.item.Item;
+import com.bedrosians.bedlogic.domain.product.Product;
+import com.bedrosians.bedlogic.domain.product.enums.DBOperation;
 import com.bedrosians.bedlogic.exception.BedDAOBadParamException;
 import com.bedrosians.bedlogic.exception.BedDAOException;
 import com.bedrosians.bedlogic.exception.BedResException;
 import com.bedrosians.bedlogic.util.FormatUtil;
+import com.bedrosians.bedlogic.util.JsonUtil;
+import com.bedrosians.bedlogic.util.JsonWrapper.AccountWrapper;
+import com.bedrosians.bedlogic.util.account.AccountFormatUtil;
 import com.bedrosians.bedlogic.util.logger.aspect.LogLevel;
 import com.bedrosians.bedlogic.util.logger.aspect.Loggable;
+import com.bedrosians.bedlogic.util.product.ImsDataUtil;
+import com.bedrosians.bedlogic.util.product.ImsValidator;
 import com.sun.jersey.core.util.MultivaluedMapImpl;
 
 
@@ -43,24 +46,18 @@ public class AccountServiceImpl implements AccountService {
 	private SessionFactory sessionFactory;	
     @Autowired
 	AccountDao accountDao;    
-   // @Autowired
-	//AccountDetailDao accountDetailDao;    
     @Autowired
-	AccountBranchDao accountBranchDao;
-   // @Autowired
-	//AccountBranchDetailDao accountBranchDetailDao;
-	
+	AccountBranchDao accountBranchDao;	
     @Autowired
 	CheckPaymentDao checkPaymentDao;
 	    	
     @Loggable(value = LogLevel.TRACE)
     @Override
-	@Transactional(readOnly=true)
-	public List<Account> getAccountsByCustomerCode(String customerCode) throws BedDAOBadParamException, BedDAOException {
+	public List<Account> getAccountsByCustomerCodeTest(String customerCode) throws BedDAOBadParamException, BedDAOException {
     	List<Account> accounts = null;
     	
     	try{
-    		accounts = accountDao.getAccountByCustomerCode(getSession(), customerCode);
+    		accounts = accountDao.getAccountByCustomerCode(customerCode);
     		//if(account != null){	         		
     		//	account.setCheckPayments(checkPaymentDao.getCheckPaymentsForAccount(id));
     		//}
@@ -76,9 +73,34 @@ public class AccountServiceImpl implements AccountService {
 	}
 	
     @Loggable(value = LogLevel.TRACE)
+    @Override
+	public List<AccountWrapper> getAccountsByCustomerCode(String customerCode) throws BedDAOBadParamException, BedDAOException {
+    	List<Account> accounts = null;
+    	
+    	try{
+    		accounts = accountDao.getAccountByCustomerCode(customerCode);
+    		//if(account != null){	         		
+    		//	account.setCheckPayments(checkPaymentDao.getCheckPaymentsForAccount(id));
+    		//}
+    	}
+    	catch(HibernateException hbe){
+			hbe.printStackTrace();
+			if(hbe.getCause() != null)
+		  	   throw new BedDAOException("Error occured during getProductById(), due to: " +  hbe.getMessage() + ". Root cause: " + hbe.getCause().getMessage());	
+		  	else
+		  	   throw new BedDAOException("Error occured during getProductById(), due to: " +  hbe.getMessage());	
+		}
+    	List<AccountWrapper> accountWrapperList = new ArrayList<AccountWrapper>();
+    	for(Account account : accounts){
+    		//accountWrapperList.add(new AccountWrapper(account));
+    		accountWrapperList.add(new AccountWrapper(AccountFormatUtil.process(account)));
+    	}
+    	return accountWrapperList;
+	}
+    
+    @Loggable(value = LogLevel.TRACE)
 	@Override
-	@Transactional(readOnly=true)
-	public List<Account> getAccounts(MultivaluedMap<String, String> queryParams) throws BedDAOException{
+	public List<AccountWrapper> getAccounts(MultivaluedMap<String, String> queryParams) throws BedDAOException{
 		if(queryParams == null || queryParams.isEmpty()){
 		   queryParams = new MultivaluedMapImpl();
 		   queryParams.put("inactivecode", Arrays.asList(new String[]{"N"}));
@@ -87,27 +109,70 @@ public class AccountServiceImpl implements AccountService {
 		try{
 			accounts = accountDao.getAccounts(queryParams);
 		}
+		catch(QueryParameterException e){
+			if(e.getCause() != null)
+		       throw new BedDAOException("Error occured during getAccounts(), due to: " +  e.getMessage() + ". Root cause: " + e.getCause().getMessage());	
+    		else
+	  		   throw new BedDAOException("Error occured during getAccounts(), due to: " +  e.getMessage());
+		}
 		catch(HibernateException hbe){
 			hbe.printStackTrace();
 			if(hbe.getCause() != null)
-		       throw new BedDAOException("Error occured during getProducts(), due to: " +  hbe.getMessage() + ". Root cause: " + hbe.getCause().getMessage());	
+		       throw new BedDAOException("Error occured during getAccounts(), due to: " +  hbe.getMessage() + ". Root cause: " + hbe.getCause().getMessage());	
 		  	else
-		  	   throw new BedDAOException("Error occured during getProducts(), due to: " +  hbe.getMessage());
+		  	   throw new BedDAOException("Error occured during getAccounts(), due to: " +  hbe.getMessage());
 		
 		}
+		catch(RuntimeException e){
+			if(e.getCause() != null)
+		  	   throw new BedDAOException("Error occured during getAccounts(), due to: " +  e.getMessage() + ". Root cause: " + e.getCause().getMessage());	
+		  	else
+		  	   throw new BedDAOException("Error occured during getAccounts(), due to: " +  e.getMessage());	
+		}
 		List<Account> processedAccounts = new ArrayList<>();
-		//for(Account account  : accounts){
-			//processedAccounts.add(FormatUtil.process(account));			
-		//}
-		return accounts;//processedAccounts;
+		 List<AccountWrapper> wrappedAccountList = new  ArrayList<AccountWrapper>();
+		for(Account account  : accounts){
+			processedAccounts.add(AccountFormatUtil.process(account));
+			wrappedAccountList.add(new AccountWrapper(AccountFormatUtil.process(account)));
+		}
+		return wrappedAccountList;//processedAccounts;
 	}
-	
+    
     @Loggable(value = LogLevel.TRACE)
 	@Override
 	@Transactional
-	public String createAccount(JSONObject inputJsonObj) throws BedDAOBadParamException, BedDAOException, BedResException{
+	public String createAccount(JSONObject inputJsonObj) throws BedDAOBadParamException, BedDAOException{
 		return accountDao.createAccount(getSession(), inputJsonObj); 
 	}
+    
+    @Loggable(value = LogLevel.TRACE)
+	@Override
+	@Transactional
+	public void updateAccount(JSONObject jsonObj) throws BedDAOBadParamException, BedDAOException{
+    	//String itemCode = JsonUtil.validateItemCode(jsonObj);
+		Account account = (Account)JsonUtil.jsonObjectToPOJO(jsonObj, new Account());
+		Session session = getSession();
+		
+  	    //ImsValidator.validateNewItem(itemToUpdate);
+    	try{
+		      accountDao.updateAccount(account);
+	 	}
+    	catch(HibernateException hbe){
+     	      if(hbe.getCause() != null)
+ 		         throw new BedDAOException("Error occured during createProduct(), due to: " +  hbe.getMessage() + ". Root cause: " + hbe.getCause().getMessage());	
+ 		      else
+ 		  	     throw new BedDAOException("Error occured during createProduct(), due to: " +  hbe.getMessage());	
+ 	    }	
+    	catch(Exception e){
+			  if(e.getMessage().contains("constraint [vendor_apv_fkey]"))
+			     throw new BedDAOBadParamException("Invalid vendor number (ID), since it cannot be found in the vendor table");
+			  if(e.getCause() != null)
+		         throw new BedDAOException("Error occured during updateProduct(), due to: " +  e.getMessage() + ". Root cause: " + e.getCause().getMessage());	
+		  	  else
+			     throw new BedDAOException("Error occured during updateProduct(), due to: " +  e.getMessage());	
+		   }	   
+	}
+	
 	
 /*	@Loggable(value = LogLevel.TRACE)
 	@Override
@@ -129,15 +194,7 @@ public class AccountServiceImpl implements AccountService {
 		  // accountDao.update((SimplifiedAccount)account); 
 	}
 	
-	@Loggable(value = LogLevel.TRACE)
-	@Override
-	@Transactional
-	public void updateAccount(Account account){
-		//if(account instanceof AccountDetail)
-		   accountDetailDao.updateAccount(account); 
-		//else if(account instanceof SimplifiedAccount)
-		  // accountDao.update((SimplifiedAccount)account);
-	}	
+		
 	
 	
 	@Loggable(value = LogLevel.TRACE)
@@ -147,10 +204,18 @@ public class AccountServiceImpl implements AccountService {
 		return accountBranchDetailDao.findById(getSession(), branchPK);
 	}
 	*/
+    @Loggable(value = LogLevel.TRACE)
+	@Override
+	public Branch getBranch(String coustomerCode, String branchCode) {
+		
+		return accountBranchDao.getBranchByBranchPK(new BranchPK(coustomerCode, branchCode));
+    	//return accountBranchDao.getAccountBranchById(coustomerCode, branchCode);
+	}
+    
 	@Loggable(value = LogLevel.TRACE)
 	@Override
-	public List<AccountBranch> getAccountBranches(String accountId) {
-		return accountBranchDao.getAccountBranches(accountId);
+	public List<Branch> getBranches(String accountId) {
+		return accountBranchDao.getBranches(accountId);
 	}
 	
 	
