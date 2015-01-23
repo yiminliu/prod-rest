d101aacaeda7b023166f71acf537c6588d9cc5c1
yiminliu <yiminliu@10.4.227.8>
2015-01-22 17:08:47 -0800

    Cleaned up code and added more comments

--- src/main/java/com/bedrosians/bedlogic/resources/v2/ImsResource.java
+++ src/main/java/com/bedrosians/bedlogic/resources/v2/ImsResource.java
@@ -1,15 +1,16 @@
 package com.bedrosians.bedlogic.resources.v2;
 
 
+import java.net.URI;
 import java.util.List;
 
-import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.GET;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.HttpHeaders;
@@ -26,13 +27,13 @@ import com.bedrosians.bedlogic.exception.BedException;
 import com.bedrosians.bedlogic.exception.BedExceptionMapper;
 import com.bedrosians.bedlogic.exception.BedResException;
 import com.bedrosians.bedlogic.exception.BedResExceptionMapper;
-import com.bedrosians.bedlogic.exception.BedResUnAuthorizedException;
 import com.bedrosians.bedlogic.models.Products;
 import com.bedrosians.bedlogic.service.ims.ImsService;
 import com.bedrosians.bedlogic.service.security.KeymarkUcUserSecurityService;
 import com.bedrosians.bedlogic.util.JsonWrapper.ItemWrapper;
 import com.bedrosians.bedlogic.util.enums.ApiName;
 import com.bedrosians.bedlogic.util.enums.DBOperation;
+import com.bedrosians.bedlogic.util.ims.ImsQueryUtil;
 import com.bedrosians.bedlogic.util.logger.aspect.LogLevel;
 import com.bedrosians.bedlogic.util.logger.aspect.Loggable;
 
@@ -40,7 +41,7 @@ import com.bedrosians.bedlogic.util.logger.aspect.Loggable;
 /**
 * This restful web service resource class acts as the logical resource of ims Service to provide database CRUD operations on ims.
 * This web service resource is used via HTTP request method (GET, POST, PUT, DELETE). JSON is the only format supported for message exchange by this resource.
-* This class uses "/ims" as its endpoint.
+* This class uses "/ims" as its root endpoint.
 *
 */
 
@@ -53,124 +54,113 @@ public class ImsResource
     private ImsService imsService;
     @Autowired
     private KeymarkUcUserSecurityService keymarkUcUserSecurityService;
+    @Autowired
+    private BedExceptionMapper bedExceptionMapper;
 
+    /** This method retrieves an item for the given item code.
+     * @param itemcode string.
+     * @return Response object which contains the status of "200, OK" and an item object in json format, or error status and message if exception occurs
+   */
+   @GET
+   @Path("{itemcode}")
+   @Produces({MediaType.APPLICATION_JSON})
+   @Loggable(value = LogLevel.INFO)
+   public Response getItem(@Context HttpHeaders requestHeaders, @PathParam("itemcode") final String itemCode)
+   {
+  	 Response response;
+  	 try
+       {
+          //Check user security
+          keymarkUcUserSecurityService.doUserSecurityCheck(requestHeaders, APINAME, DBOperation.SEARCH);
+          //Retrieve data from database based on the given item code
+          Ims item = imsService.getItem(itemCode);
+          //Wrap the data
+          Products result = new Products(new ItemWrapper(item));
+          String jsonStr = result.toJSONStringWithJackson("ims");
+          //Create json response
+          response = Response.ok(jsonStr, MediaType.APPLICATION_JSON).build();
+        }
+        catch (BedException e)
+        {
+          response = bedExceptionMapper.toResponse(e);
+        }
+        catch (BedResException e)
+        {
+           response = BedResExceptionMapper.MapToResponse(e);
+        }
+  	  return response;
+    }
+   
     /**
       * This method retrieves a list of items for the given query condition, or a list of all active items if no query condition is specified .
       * @param UriInfo represents query condition in the form of name/value pairs. If no query is specified, all active items will be returned.
       * Number of resulting records can be specified by setting a value for "maxResults" and if "exactmatch" is set to true, no pattern matching will be performed for all queries.
-      * @return Response object contains the status and a json object.
-      * @exception.
-     */
+      * @return Response object which contains the status of "200, OK" and a list of items in json format, or error status and message if exception occurs
+      */
      @GET
      @Produces({MediaType.APPLICATION_JSON})
      @Loggable(value = LogLevel.INFO)
      public Response getItems(@Context HttpHeaders requestHeaders, @Context UriInfo uriInfo)
-     {
+     { 
+    	List<?> itemList = null;
         Response response = null;
-
+        Products result = null;
+        String jsonStr = null;
         try
         {
             //Check user security
             keymarkUcUserSecurityService.doUserSecurityCheck(requestHeaders, APINAME, DBOperation.SEARCH);
-
             //Retrieve data from database based on the given query parameters
-            List<ItemWrapper> wrappedItemList = imsService.getWrappedItems(uriInfo.getQueryParameters());//new ArrayList<ProductWrapper>();
-
-            //Convert the data to Json string
-            Products result = new Products(wrappedItemList);
-            String jsonStr = result.toJSONStringWithJackson();
-
-            //Return json reponse
+            String wrappedData = ImsQueryUtil.getValue(uriInfo.getQueryParameters(), "wrappedData");
+            if("No".equalsIgnoreCase(wrappedData)){
+            	itemList = imsService.getItems(uriInfo.getQueryParameters(), false);
+            	//Convert the data to Json string
+            	result = new Products(itemList);
+               	jsonStr = result.toJSONStringWithJackson(null);
+            }
+            else{
+            	itemList = imsService.getItems(uriInfo.getQueryParameters(), true);
+            	//Convert the data to Json string
+            	result = new Products(itemList);
+            	jsonStr = result.toJSONStringWithJackson("ims");
+            }
+            //Create json response
             response = Response.ok(jsonStr, MediaType.APPLICATION_JSON).build();
         }
         catch (BedException e)
         {
-           response = BedExceptionMapper.MapToResponse(e);
+           response = bedExceptionMapper.toResponse(e);
         }
         catch (BedResException e)
         {
             response = BedResExceptionMapper.MapToResponse(e);
         }
-        
         return response;
      }
-
-     
-     /** This method retrieves an item for the given item code.
-       * @param itemcode string.
-       * @return Response object to include the status and a json object.
-       * @exception
-     */
-     @GET
-     @Path("{itemcode}")
-     @Produces({MediaType.APPLICATION_JSON})
-     @Loggable(value = LogLevel.INFO)
-     public Response getItemsByItemCode(@Context HttpHeaders requestHeaders, @PathParam("itemcode") final String itemCode)
-     {
-
-    	 Response response;
-
-    	 try
-         {
-            //Check user security
-            keymarkUcUserSecurityService.doUserSecurityCheck(requestHeaders, APINAME, DBOperation.SEARCH);
-
-            //Retrieve data from database based on the give id
-            Ims ims = imsService.getItemByItemCode(itemCode);
-            Products result = new Products(ims);
-            String jsonStr = result.toJSONStringWithJacksonWithRootName();
-
-            //Return json reponse
-            response = Response.ok(jsonStr, MediaType.APPLICATION_JSON).build();
-          }
-          catch (BedException e)
-          {
-            response = BedExceptionMapper.MapToResponse(e);
-          }
-          catch (BedResException e)
-          {
-             response = BedResExceptionMapper.MapToResponse(e);
-          }
-
-    	  return response;
-      }
 
      /**
        * This method creates a item based on the given information.
        * @param Json object containing the information to create a new item.
-       * @return Json object representing the status and created item code.
-       * @exception
+       * @return Response object which contains the status of "201, created" and an uri with the created item code, or error status and message if exception occurs
        */
        @POST
-       @Produces(MediaType.APPLICATION_JSON)
        @Consumes(MediaType.APPLICATION_JSON)
        @Loggable(value = LogLevel.INFO)
        public Response createItem(@Context HttpHeaders requestHeaders, JSONObject inputJsonObj)
        {
           Response response;
-
           try
           {
              //Check user security
              keymarkUcUserSecurityService.doUserSecurityCheck(requestHeaders, APINAME, DBOperation.CREATE);
-
              //Create a new item using the given data in json format, and save it into database
              String itemCode = imsService.createItem(inputJsonObj);
-
-             //Wrape the newly created item code into json
-             Products result = new Products(itemCode);
-             String jsonStr = result.toJSONStringWithJackson();
-
-             //Return json reponse
-             response = Response.ok(jsonStr, MediaType.APPLICATION_JSON).build();
+             //Create response
+             response = Response.created(URI.create("/"+itemCode)).build();
           }
           catch (BedException e)
           {
-             response = BedExceptionMapper.MapToResponse(e);
-          }
-          catch (BedResException e)
-          {
-              response = BedResExceptionMapper.MapToResponse(e);
+             response = bedExceptionMapper.toResponse(e);
           }
           return response;
        }
@@ -178,11 +168,9 @@ public class ImsResource
      /**
        * This method updates an item based on the given item info.
        * @param A Json object containing item information to update.
-       * @return Response object to include the status.
-       * @exception
+       * @return Response object which contains a "204, No Content" status and no message body, or error status and message if exception occurs
        */
        @PUT
-       @Produces(MediaType.APPLICATION_JSON)
        @Consumes(MediaType.APPLICATION_JSON)
        @Loggable(value = LogLevel.INFO)
        public Response updateItem(@Context HttpHeaders requestHeaders, JSONObject inputJsonObj)
@@ -190,83 +178,45 @@ public class ImsResource
           Response response;
           try
           {
-              //Check user security
-              keymarkUcUserSecurityService.doUserSecurityCheck(requestHeaders, APINAME, DBOperation.UPDATE);
-              //Update an item based on the input json data
-              imsService.updateItem(inputJsonObj);
-
-              // Return json reponse
-              response = Response.ok(MediaType.APPLICATION_JSON).build();
-          }
-          catch (BedException e)
-          {
-             response = BedExceptionMapper.MapToResponse(e);
-          }
-           
-          return response;
-       }
-
-     /**
-       * This method deletes an item based on the given item code.
-       * @param A Json object containing product information to update.
-       * @return Json object representing the status.
-       * @exception
-       */
-       @DELETE
-       @Produces({MediaType.APPLICATION_JSON})
-       @Consumes(MediaType.APPLICATION_JSON)
-       @Loggable(value = LogLevel.INFO)
-       public Response deleteItem(@Context HttpHeaders requestHeaders, JSONObject inputJsonObj)
-       {
-          Response response;
-          try
-          {
              //Check user security
-             keymarkUcUserSecurityService.doUserSecurityCheck(requestHeaders, APINAME, DBOperation.DELETE);
-             //delete an item from database with the given item info
-             imsService.deleteItem(inputJsonObj);
-             //Return json reponse
-             response = Response.ok(MediaType.APPLICATION_JSON).build();
+             keymarkUcUserSecurityService.doUserSecurityCheck(requestHeaders, APINAME, DBOperation.UPDATE);
+             //Update an item based on the input json data
+             imsService.updateItem(inputJsonObj);
+             //Create response
+             response = Response.noContent().build();
           }
           catch (BedException e)
           {
-             response = BedExceptionMapper.MapToResponse(e);
+             response = bedExceptionMapper.toResponse(e);
           }
-             
           return response;
        }
 
      /**
        * This method deletes an item based on the given item code.
        * @param Item code string.
-       * @return Json object representing the status.
-       * @exception
+       * @return Response object which contains a "204, No Content" status and no message body, or error status and message if exception occurs
        */
        @DELETE
        @Path("{itemcode}")
-       @Produces({MediaType.APPLICATION_JSON})
+       @Consumes(MediaType.APPLICATION_JSON)
        @Loggable(value = LogLevel.INFO)
-       public Response deleteItemByItemCode(@Context HttpHeaders requestHeaders, @PathParam("itemcode") final String itemCode)
+       public Response deleteItem(@Context HttpHeaders requestHeaders, @PathParam("itemcode") final String itemCode)
        {
           Response response;
           try
           {
              //Check user security
              keymarkUcUserSecurityService.doUserSecurityCheck(requestHeaders, APINAME, DBOperation.DELETE);
-
              //delete an item from database based on the given item code
              imsService.deleteItemByItemCode(itemCode);
-
-             //Return json reponse
-             //response = Response.ok(MediaType.APPLICATION_JSON).build();
+             //Create response
              response = Response.noContent().build();
           }
           catch (BedException e)
           {
-             response = BedExceptionMapper.MapToResponse(e);
+             response = bedExceptionMapper.toResponse(e);
           }
-                
           return response;
        }
-
 }
