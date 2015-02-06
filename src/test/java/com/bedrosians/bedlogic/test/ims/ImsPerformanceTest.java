1d35919fc60b6aa58276a45350011ffa537ccf97
yiminliu <yiminliu@10.4.227.8>
2015-02-06 14:48:55 -0800

    Removed unused code

--- src/test/java/com/bedrosians/bedlogic/test/ims/ImsPerformanceTest.java
+++ src/test/java/com/bedrosians/bedlogic/test/ims/ImsPerformanceTest.java
@@ -1,7 +1,6 @@
 package com.bedrosians.bedlogic.test.ims;
 
 import java.util.Arrays;
-import java.util.List;
 
 import javax.ws.rs.core.MultivaluedMap;
 
@@ -15,10 +14,8 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.transaction.TransactionConfiguration;
 
 import com.bedrosians.bedlogic.dao.ims.ImsDao;
-import com.bedrosians.bedlogic.domain.ims.Ims;
 import com.bedrosians.bedlogic.service.ims.ImsService;
 import com.bedrosians.bedlogic.service.user.KeymarkUcUserService;
-import com.bedrosians.bedlogic.util.JsonWrapper.ItemWrapper;
 import com.sun.jersey.core.util.MultivaluedMapImpl;
 
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -50,12 +47,11 @@ public class ImsPerformanceTest {
 		params.put("origin", Arrays.asList(new String[]{"USA"}));
 		params.put("maxResults", Arrays.asList(new String[]{"500"}));
 		
-		List<ItemWrapper> products = null;
 	    for(int i = 0; i< 100; i++){
 	    	try{
 	           Session session = sessionFactory.openSession();
 	           session.getTransaction().begin();
-	           products = (List<ItemWrapper>)imsService.getItems(params, true);
+	           imsService.getItems(params, true);
                session.getTransaction().commit();
                session.close();
 	        }
